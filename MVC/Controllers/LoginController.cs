ea1f11402ba34c7280c6219a7c76cde257e914b3
admin <admin@DESKTOP-O74C053>
2022-06-21 20:51:53 +0530

    Add project files.

---
+++ MVC/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Login login)
+        {
+            if (login != null && login.UName == "aaa" && login.Password == "aaa")
+            {
+                return RedirectToAction("Index", "Employees", login);
+            }
+            else return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
+        }
+    }
+}
